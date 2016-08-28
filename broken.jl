563843ddc2728cd6aff1e246503f0c71b6f717b3
Benjamin Chung <bwchung@andrew.cmu.edu>
2016-08-28 16:25:07 -0400

    More figure 2 fixes

--- broken.jl
+++ broken.jl
@@ -1,12 +1,15 @@
 abstract A1
 type C1 <: A1 end
+type C2 <: A1 end
 abstract A2 <: A1
 type C3 <: A2 end
 
-function example(arg::C1) print("Hello\n") end
+function example(arg::C1) print("Hello ") end
+function example(arg::C2) print("World\n") end
 function problem(arg::A1)
   return example(arg)
 end
 
-problem(C1()) #prints Hello
+problem(C1()) #Hello
+problem(C2()) #World
 problem(C3()) #error
