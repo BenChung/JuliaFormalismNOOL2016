00d2f322ac9ea8bd857ac0307e63398d93905e7d
Benjamin Chung <bwchung@andrew.cmu.edu>
2016-08-28 15:51:26 -0400

    Fixed code example

--- broken.jl
+++ broken.jl
@@ -3,10 +3,10 @@ type C1 <: A1 end
 abstract A2 <: A1
 type C3 <: A2 end
 
-function example(arg::C1) end
+function example(arg::C1) print("Hello\n") end
 function problem(arg::A1)
   return example(arg)
 end
 
-problem(C1()) # works
-problem(C3()) # breaks
+problem(C1()) #prints Hello
+problem(C3()) #error
